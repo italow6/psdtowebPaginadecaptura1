8843d43e42f2b51fdb5b793328839d8a773783b1
Ítalo Iran <italow6@gmail.com>
2022-03-31 12:42:55 -0300

    first commit

---
+++ envio.php
@@ -0,0 +1,27 @@
+<?
+
+# alterar a variavel abaixo colocando o seu email
+
+$destinatario = "seuemail@seudominio.com.br";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+$assunto = $_REQUEST['assunto'];
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "Informações" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Zap: " . $zap . "\n";
+$body = $body . "Email: " . $email . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página 2
+header("location:page2.html");
+
+
+?>
